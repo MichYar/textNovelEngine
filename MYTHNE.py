305ff1185a8130ce3217085f4d8d72d235c6d300
Michael Yarikov <59765252+MichYar@users.noreply.github.com>
2021-10-22 20:04:25 +0300

    Update MYTHNE.py
    added any key pressed waiting

--- MYTHNE.py
+++ MYTHNE.py
@@ -1,8 +1,11 @@
+import os
 def linear(words):
     for _ in words:
          print(_)
+         os.system("pause")
 def choice(choices, choose_times=1):
     for _ in range(choose_times):
         choice = input()
         if choice in choices:
             linear(choices[choice])
+            
